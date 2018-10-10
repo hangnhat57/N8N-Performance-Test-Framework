b61b524bb952ca932efe9422ea62c096ad01dc66
Nhat Nguyen <nhat@Akita-3.local>
2018-10-10 15:46:34 +0700

    First push

---
+++ influxdb/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+if [ ! -f "/var/lib/influxdb/.init" ]; then
+    exec influxd $@ &
+
+    until wget -q "http://localhost:8086/ping" 2> /dev/null; do
+        sleep 1
+    done
+
+    influx -host=localhost -port=8086 -execute="CREATE USER ${INFLUX_USER} WITH PASSWORD '${INFLUX_PASSWORD}' WITH ALL PRIVILEGES"
+    influx -host=localhost -port=8086 -execute="CREATE DATABASE ${INFLUX_DB}"    
+    influx -host=localhost -port=8086 -execute="CREATE DATABASE collectd"    
+    
+    touch "/var/lib/influxdb/.init"
+
+    kill -s TERM %1
+fi
+
+exec influxd $@
