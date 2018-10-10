63b1c81edef89f5737989fd272329f90375c3d0f
N8N <hangnhat57@gmail.com>
2018-10-10 17:50:20 +0700

    Fix bad syntax for collectd

--- install-collectd.sh
+++ install-collectd.sh
@@ -1,4 +1,12 @@
-#!bin/bash
-apt-get update && apt-get install collectd collectd-utils -y
-cp collectd.conf /etc/collectd/collectd.conf
-service collectd restart
+#!/bin/bash
+set -e
+#Install collectd
+if [ $(dpkg-query -W -f='${Status}' collectd 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  sudo apt-get update && sudo apt-get install collectd collectd-utils -y
+fi
+#Copy config file for collectd
+echo "Please remember change your host"
+sudo cp collectd.conf /etc/collectd/collectd.conf
+#Restart service
+sudo service collectd restart
