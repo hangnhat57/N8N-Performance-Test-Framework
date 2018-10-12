f23763d7e9a5828d1cd1dd35f93d796c21df8687
root <nhat.nguyen@twentyci.co.uk>
2018-10-12 14:41:21 +0700

    Update private wpt server

--- run-sitespeed.sh
+++ run-sitespeed.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 url=$1
 args=$2
-if [ -z "$args"]
+config=$3
+if [ -z "$args" ]
   then
     echo "Start execute sitespeed test for ${url} with no agruments!"
-    docker-compose run --rm sitespeed.io --config=./config.json ${url}
+    docker-compose run --rm sitespeed.io --config=./${config:-"config.json"} ${url}
   else
     echo "Start execute sitespeed test for ${url} with agruments: ${args}"
-    docker-compose run --rm sitespeed.io --config=./config.json ${url} ${args}
+    docker-compose run --rm sitespeed.io --config=./${config:-"config.json"} ${url} ${args}
 fi
--- sitespeed/pre.js
+++ sitespeed/pre.js
@@ -3,7 +3,7 @@ module.exports = {
   run(context) {
     return context.runWithDriver((driver) => {
       // Go to Wikipedias login URL
-      return driver.get('http://tw-ea.twentyci.asia')
+      return driver.get('<ENTER_BASEURL>')
         .then(() => {
 
           // we fetch the selenium webdriver from context
@@ -13,9 +13,9 @@ module.exports = {
           const By = webdriver.By;
 
           // before you start, make your username and password
-          const userName = '<username>';
-          const password = '<password>';
-
+          const userName = '<ENTER_USERNAME>';
+          const password = '<ENTER_PASSWORD>';
+          driver.wait(until.elementLocated(By.id('email')),3600);
           driver.findElement(By.id('email')).sendKeys(userName);
           driver.findElement(By.id('password')).sendKeys(password);
           const loginButton = driver.findElement(webdriver.By.xpath("//button[contains(.,'Login')]"));
